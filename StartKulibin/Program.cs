3c73676e83c879551c9b6abf7fccb313f93308a9
Кошкарева Елизавета Дмитриевна <koshe@omega.local>
2023-01-19 14:22:35 +0300

    Console app  to launch the application

---
+++ StartKulibin/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Diagnostics;
+
+var appdata_var = Environment.GetEnvironmentVariable("APPDATA");
+var install_path = appdata_var + "\\Kulibin\\Kulibin.exe";
+Process prc = new Process(); 
+prc.StartInfo.FileName = install_path; 
+prc.Start();
