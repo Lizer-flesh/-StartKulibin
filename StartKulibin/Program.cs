6699f59b49328bb3c4c58d1f04feeb6b346e9980
Кошкарева Елизавета Дмитриевна <koshe@omega.local>
2023-03-10 19:51:37 +0300

    Start from local folder

--- StartKulibin/Program.cs
+++ StartKulibin/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Diagnostics;
 
 // var appdata_var = Environment.GetEnvironmentVariable("APPDATA");
-var install_path = ".\\Kulibin.exe";
+// var install_path = ".\\Kulibin.exe";
 
 // var install_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Kulibin.exe");
-Process prc = new Process(); 
-prc.StartInfo.FileName = install_path; 
-prc.Start();
-Console.ReadKey();
+// Process prc = new Process(); 
+// prc.StartInfo.FileName = install_path; 
+// prc.Start();
+// Console.ReadKey();
 
 
 // class FindKulibin
@@ -31,3 +32,25 @@ Console.ReadKey();
 //         }
 //     }
 // }
+
+string folderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+string[] pathParts = folderPath.Split(Path.DirectorySeparatorChar);
+Array.Resize(ref pathParts, pathParts.Length-5);
+string fullPath = Path.Combine(pathParts);
+
+var executePath = Path.Combine(fullPath, "Kulibin.dll");
+Process prc = new Process();
+prc.StartInfo.FileName = executePath;
+prc.Start();
+
+Console.WriteLine(fullPath);
+
+// DirectoryInfo perDir = Directory.GetParent(folderPath);
+// string perPath = perDir.FullName;
+//
+// Console.WriteLine(perPath);
+// string appFilename = "Kulibin.exe";
+// string appFullPath = Path.Combine(perPath, appFilename);
+//
+// Process.Start(appFullPath);
